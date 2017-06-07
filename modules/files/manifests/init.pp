3d745461a95a678bc5502e2f4ee691bf33814f3a
dnase <dnase@puppetlabs.vm>
2017-06-07 19:10:53 +0000

    lab 8.1 - manage files

---
+++ modules/files/manifests/init.pp
@@ -0,0 +1,30 @@
+class files {
+  file { ['/etc/cron.allow', '/etc/cron.deny']:
+    ensure => file,
+  }
+  file_line { 'root cron':
+    ensure => present,
+    path   => '/etc/cron.allow',
+    line   => 'root',
+  }
+  file_line { 'non-root cron':
+    ensure => present,
+    path   => '/etc/cron.deny',
+    line   => '*',
+  }
+  concat { '/etc/motd':
+    owner => 'root',
+    group => 'root',
+    mode  => '0644',
+  }
+  concat::fragment { 'motd header':
+    target  => '/etc/motd',
+    order   => '01',
+    content => "Check out this awesome header, yo!\n",
+  }
+  concat::fragment { 'motd message':
+    target  => '/etc/motd',
+    order   => '02',
+    content => "This is the message of the day.\n",
+  }
+}
